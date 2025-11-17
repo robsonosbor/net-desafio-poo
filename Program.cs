24621b40eeb62ce83d86939da40df5bed3c5b0db
Robson <nosboro@gmail.com>
2025-11-17 16:49:18 -0300

    create screenshot projetc

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Modelo Nokia", "12121212", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iPhone = new Iphone("987654321", "Modelo iPhone", "45454545", 128);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("ChatGPT");
